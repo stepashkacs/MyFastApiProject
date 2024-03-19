d95163cea0c3ca238f35dfe37632b6864ce8d05e
stepashkacs <cociorvastefan@gmail.com>
2024-03-19 22:32:12 +0200

    repair problem with logic views

--- api_v1/products/views.py
+++ api_v1/products/views.py
@@ -4,18 +4,18 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from core.models import db_helper
 from . import crud
 from .schemas import ProductCreate, Product
-
+from .dependecies import product_by_id
 
 
 router = APIRouter(tags=['Products'])
 
 
-@router.get('/', response_model=list[Product])
+@router.get("/", response_model=list[Product])
 async def get_products(
     # session: AsyncSession = Depends(db_helper.session_dependency), можем использовать и так или с другой зависимостю
     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
-    return await crud.get_product(session=session)
+    return await crud.get_products(session=session)
 
 @router.post('/', response_model=Product)
 async def create_product(
@@ -24,15 +24,8 @@ async def create_product(
 ):
     return await crud.create_product(session=session, product_in=product_in)
 
-@router.get('/{product_id}/', response_model=Product)
-async def get_products(
-    product_id: int,
-    session: AsyncSession = Depends(db_helper.session_dependency)
+@router.get("/{product_id}/", response_model=Product)
+async def get_product(
+    product: Product = Depends(product_by_id),
 ):
-    product = await crud.get_product(session=session, product_id=product_id)
-    if product is not None:
-        return product
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f'Product {product_id} is not found!'
-    )
+    return product
